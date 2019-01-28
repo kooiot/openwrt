4fad681c2321e9f1e0aa024099d40c5946fb017d
Dirk Chang <dirk@kooiot.com>
2019-01-28 13:13:51 +0800

    support orangepi one pick the musl fflush deadlock path
    Signed-off-by: Dirk Chang <dirk@kooiot.com>

--- target/linux/sunxi/image/cortex-a7.mk
+++ target/linux/sunxi/image/cortex-a7.mk
@@ -149,6 +149,16 @@ endef
 TARGET_DEVICES += sun8i-h3-orangepi-pc
 
 
+define Device/sun8i-h3-orangepi-one
+  DEVICE_TITLE:=Xunlong Orange Pi One
+  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-gpio-button-hotplug
+  SUPPORTED_DEVICES:=xunlong,orangepi-one
+  SUNXI_DTS:=sun8i-h3-orangepi-one
+endef
+
+TARGET_DEVICES += sun8i-h3-orangepi-one
+
+
 define Device/sun8i-h3-orangepi-plus
   DEVICE_TITLE:=Xunlong Orange Pi Plus
   DEVICE_PACKAGES:=kmod-rtc-sunxi
