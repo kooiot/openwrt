c55744f5d21b447df23cee093b766fd4aef88a39
Dirk Chang <dirk@kooiot.com>
2020-11-12 10:39:05 +0800

    enable hdmi and dsi in tlink-k1

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -254,7 +254,8 @@ define Device/kooiot_tlink-k1
     uqmi fdisk usbutils freeioe wwanleds \
     luci-app-freeioe luci-proto-qmi luci-proto-3g \
     tinc_freeioe_tunnel ser2net shellinabox iperf3 ethtool \
-    blockd kmod-usb-storage
+    blockd kmod-usb-storage kmod-drm-sunxi \
+	usb-otg-sunxi kmod-usb-gadget-serial
   SOC := sun8i-r40
 endef
 TARGET_DEVICES += kooiot_tlink-k1
