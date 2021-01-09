f2c9c643ce3fda269d1ff22867f699ad16835da8
Dirk Chang <dirk@kooiot.com>
2021-01-09 14:57:33 +0800

    cortext-a7 for imx6 ull

---
+++ target/linux/imx6/cortexa7/target.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 Dirk Chang
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=Cortex-A7
+CPU_TYPE:=cortex-a7
+CPU_SUBTYPE:=neon-vfpv4
---
+++ target/linux/imx6/cortexa9/target.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 Dirk Chang
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=Cortex-A9
+CPU_TYPE:=cortex-a9
+CPU_SUBTYPE:=neon
