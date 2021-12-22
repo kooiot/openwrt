1265439c619c54a8d0b520f59a937c898180250d
Dirk Chang <dirk@kooiot.com>
2021-12-22 18:33:12 +0800

    fixed phy chips led issue

---
+++ target/linux/generic/files/drivers/net/phy/motorcomm/yt8512c.c
@@ -0,0 +1,214 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Driver for Motorcomm YT8512C
+ *
+ * Author: Dirk Chang <dirk@kooiot.com>
+ *
+ * Copyright (c) 2021 KooIoT.COM
+ */
+
+#include <linux/delay.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mii.h>
+#include <linux/phy.h>
+#include <linux/netdevice.h>
+#include <linux/of.h>
+
+/* YT8512 phy identifier values */
+#define YT8512_PHY_ID	0x00000128
+
+#define YT8512_SPEC_STS_REG		0x1E
+#define YT8512_EXT_ADDR_REG		0x1E
+#define YT8512_EXT_DATA_REG		0x1F
+
+#define YT8512_10BT_BUG_REG		0x200A
+#define YT8512_LED0_CFG_REG		0x40C0
+#define YT8512_LED1_CFG_REG		0x40C3
+
+#define PHY_ID_HACKS 1
+#define MDIO_TIMEOUT		(msecs_to_jiffies(500))
+
+struct yt8512_phy_priv {
+	bool use_led0;
+	u16 led0;
+	bool use_led1;
+	u16 led1;
+};
+
+static int yt_ext_read(struct phy_device *phydev, u16 reg) {
+	int ret;
+	ret = phy_write(phydev, YT8512_EXT_ADDR_REG, reg);
+	if (ret < 0)
+		return ret;
+
+	// usleep_range(20, 50);
+	msleep(1);
+
+	return phy_read(phydev, YT8512_EXT_DATA_REG);
+}
+
+static int yt_ext_write(struct phy_device *phydev, u16 reg, u16 data) {
+	int ret;
+	unsigned long timeout_jiffies;
+	ret = phy_write(phydev, YT8512_EXT_ADDR_REG, reg);
+	if (ret < 0)
+		return ret;
+
+	// usleep_range(20, 50);
+	msleep(1);
+
+	ret = phy_write(phydev, YT8512_EXT_DATA_REG, data);
+
+	/* Wait read complete */
+	timeout_jiffies = jiffies + MDIO_TIMEOUT;
+	do {
+		if (time_is_before_jiffies(timeout_jiffies))
+			return -ETIMEDOUT;
+
+		msleep(1);
+
+		ret = yt_ext_read(phydev, reg);
+
+		// phydev_info(phydev, "r:%x - %x", ret, data);
+	} while (ret != data);
+
+	return ret;
+}
+
+static int yt8512_probe(struct phy_device *phydev) {
+	struct device *dev = &phydev->mdio.dev;
+	struct device_node *of_node = dev->of_node;
+	struct yt8512_phy_priv *priv;
+	int reg;
+
+	priv = devm_kzalloc(dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	if (of_property_read_u32(of_node, "motorcomm,led0", &reg)) {
+		priv->use_led0 = false;
+		phydev_info(phydev, "no led0 config val");
+	} else {
+		priv->use_led0 = true;
+		priv->led0 = (u16)reg;
+		phydev_info(phydev, "led0 config val: %x of_node:%pOF", priv->led0, of_node);
+	}
+
+	if (of_property_read_u32(of_node, "motorcomm,led1", &reg)) {
+		priv->use_led1 = false;
+		phydev_info(phydev, "no led1 config val");
+	} else {
+		priv->use_led1 = true;
+		priv->led1 = (u16)reg;
+		phydev_info(phydev, "led1 config val: %x of_node:%pOF", priv->led1, of_node);
+	}
+
+	phydev->priv = priv;
+
+	return 0;
+}
+
+static int yt8512_led_init(struct phy_device *phydev)
+{
+	int ret = 0;
+	struct yt8512_phy_priv *priv = phydev->priv;
+
+	ret = yt_ext_read(phydev, YT8512_LED0_CFG_REG);
+	phydev_info(phydev, "read led0 config val: %x", ret);
+
+	if (priv->use_led0) {
+		phydev_info(phydev, "write led0 config val: %x", priv->led0);
+		ret = yt_ext_write(phydev, YT8512_LED0_CFG_REG, priv->led0);
+		if (ret < 0) {
+			phydev_err(phydev, "write led0 config failed!\n");
+			goto error_return;
+		}
+
+		ret = yt_ext_read(phydev, YT8512_LED0_CFG_REG);
+		phydev_info(phydev, "after write led0 config val: %x", ret);
+	}
+
+	ret = yt_ext_read(phydev, YT8512_LED1_CFG_REG);
+	phydev_info(phydev, "read led1 config val: %x", ret);
+
+	if (priv->use_led1) {
+		phydev_info(phydev, "write led1 config val: %x", priv->led1);
+		ret = yt_ext_write(phydev, YT8512_LED1_CFG_REG, priv->led1);
+		if (ret < 0) {
+			phydev_err(phydev, "write led1 config failed!\n");
+			goto error_return;
+		}
+
+		ret = yt_ext_read(phydev, YT8512_LED1_CFG_REG);
+		phydev_info(phydev, "after write led1 config val: %x", ret);
+	}
+
+error_return:
+	return ret;
+}
+
+static int yt8512_config_init(struct phy_device *phydev)
+{
+	int ret;
+	struct yt8512_phy_priv *priv = phydev->priv;
+	if (!priv)
+		return 0;
+
+#ifdef PHY_ID_HACKS
+	ret = phy_read(phydev, MII_PHYSID1) << 16;
+	ret |= phy_read(phydev, MII_PHYSID2);
+	phydev_info(phydev, "%s: phy_id: %x", __func__, ret);
+	if (ret != YT8512_PHY_ID)
+		return 0;
+#endif
+
+	// 10BT bug?
+	ret = yt_ext_read(phydev, YT8512_10BT_BUG_REG);
+	if (ret < 0) {
+		phydev_err(phydev, "read 10BT BUG REG failed!\n");
+		goto error_return;
+	} else {
+		phydev_info(phydev, "10BT BUG REG val: %x", ret);
+	}
+
+	if (0) {
+		ret = yt_ext_write(phydev, YT8512_10BT_BUG_REG, ret & 0xFBFF);
+		if (ret < 0) {
+			phydev_err(phydev, "write 10BT BUG REG failed!\n");
+			goto error_return;
+		}
+	}
+
+	ret = yt8512_led_init(phydev);
+	if (ret < 0) {
+		goto error_return;
+	}
+
+	return 0;
+
+error_return:
+	return ret;
+}
+
+static struct phy_driver yt8512c_driver[] = { {
+	.name = "MOTORCOMM YT8512C",
+	.phy_id = YT8512_PHY_ID,
+	.phy_id_mask = 0xfffffff0,
+	.features = PHY_BASIC_FEATURES,
+	.probe = yt8512_probe,
+	.config_init = yt8512_config_init,
+} };
+
+module_phy_driver(yt8512c_driver);
+
+MODULE_DESCRIPTION("MOTORCOMM YT8512C PHY driver");
+MODULE_AUTHOR("Dirk Chang");
+MODULE_LICENSE("GPL");
+
+static struct mdio_device_id __maybe_unused phy_tbl[] = {
+	{ YT8512_PHY_ID, 0xfffffff0 },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(mdio, phy_tbl);
