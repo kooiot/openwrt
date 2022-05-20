42c34b38587419328b36f01209655cd8bf55a3ad
Dirk Chang <dirk@kooiot.com>
2022-05-20 20:53:29 +0800

    rockchip: rename our board to be shorter name

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -51,11 +51,11 @@ define Device/kooiot_tlink-rk3568
 endef
 TARGET_DEVICES += kooiot_tlink-rk3568
 
-define Device/kooiot_tlink-rockchip-r5
+define Device/kooiot_tlink-r5
   DEVICE_VENDOR := KooIoT
-  DEVICE_MODEL := ThingsLink R5 (RK3568)
+  DEVICE_MODEL := ThingsLink R5
   SOC := rk3568
-  UBOOT_DEVICE_NAME := tlink-rockchip-r5
+  UBOOT_DEVICE_NAME := tlink-r5
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
-TARGET_DEVICES += kooiot_tlink-rockchip-r5
+TARGET_DEVICES += kooiot_tlink-r5
