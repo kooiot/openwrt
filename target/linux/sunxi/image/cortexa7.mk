147ec3533116090c5b3d2be1f6bc5e10e52f3afd
Dirk Chang <dirk@kooiot.com>
2022-07-06 12:15:02 +0800

    sunxi: can bus and gpio export of TLink K2X

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -522,6 +522,7 @@ define Device/kooiot_tlink-k2x
     kmod-usb-serial-qualcomm kmod-usb-net-qmi-wwan \
     kmod-usb-net-asix kmod-serial-wk2xxx-spi \
 	kmod-can kmod-can-mcp251x \
+	kmod-can-bcm kmod-can-raw ip-full\
 	kmod-motorcomm-yt8521s kmod-motorcomm-yt8512c \
     uqmi fdisk usbutils freeioe wwanleds \
     luci-app-freeioe luci-proto-qmi luci-proto-3g \
