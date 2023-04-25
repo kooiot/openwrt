506e8b1260c019ff9c047f4e198dd1d466c3284a
Dirk Chang <dirk@kooiot.com>
2023-04-25 17:48:17 +0800

    linux-firmware: added firmware for tlink r7

--- package/firmware/linux-firmware/broadcom.mk
+++ package/firmware/linux-firmware/broadcom.mk
@@ -147,3 +147,13 @@ define Package/tlink-r4x-firmware/install
 	$(INSTALL_DATA) ./files/broadcom/AP6275S/wifi/nvram_ap6275s.txt $(1)/lib/firmware/brcm/brcmfmac43752-sdio.kooiot,tlink-r4x.txt
 endef
 $(eval $(call BuildPackage,tlink-r4x-firmware))
+
+Package/tlink-r7-firmware = $(call Package/firmware-default,Broadcom FullMac SDIO firmware)
+define Package/tlink-r7-firmware/install
+	$(INSTALL_DIR) $(1)/lib/firmware/brcm
+	$(INSTALL_DATA) ./files/broadcom/AP6275S/bt/BCM4362A2.hcd $(1)/lib/firmware/brcm/BCM4362A2.hcd
+	$(INSTALL_DATA) ./files/broadcom/AP6275S/wifi/clm_bcm43752a2_ag.blob $(1)/lib/firmware/brcm/brcmfmac43752-sdio.clm_blob
+	$(INSTALL_DATA) ./files/broadcom/AP6275S/wifi/fw_bcm43752a2_ag_apsta.bin $(1)/lib/firmware/brcm/brcmfmac43752-sdio.kooiot,tlink-r7.bin
+	$(INSTALL_DATA) ./files/broadcom/AP6275S/wifi/nvram_ap6275s.txt $(1)/lib/firmware/brcm/brcmfmac43752-sdio.kooiot,tlink-r7.txt
+endef
+$(eval $(call BuildPackage,tlink-r7-firmware))
