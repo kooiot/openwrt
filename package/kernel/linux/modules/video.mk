26817483028e41a883aae63c513344d61a2a10d0
Dirk Chang <dirk@kooiot.com>
2023-10-11 19:38:50 +0800

    modules: added drm display helper depends with kernel-6.1

--- package/kernel/linux/modules/video.mk
+++ package/kernel/linux/modules/video.mk
@@ -365,7 +365,8 @@ define KernelPackage/drm-sun8i
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=SUN8I SOCs DRM support
   DEPENDS:=@TARGET_sunxi @DISPLAY_SUPPORT \
-	  +kmod-drm +kmod-drm-kms-helper +kmod-lib-crc-ccitt
+    +kmod-drm +kmod-drm-kms-helper +kmod-lib-crc-ccitt \
+    +LINUX_6_1:kmod-drm-display-helper
   KCONFIG:= \
     CONFIG_DRM_SUN4I \
 	CONFIG_MEDIA_CEC_SUPPORT=y \
