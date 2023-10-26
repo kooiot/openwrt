261cf54c4ee95d7d9e0cbc9e5c6b0b98cd8feaad
Dirk Chang <dirk@kooiot.com>
2023-10-26 11:51:06 +0800

    prereq: using pyenv to fix python2.7 missing issue

---
+++ pyenv_for_py2.sh
@@ -0,0 +1,7 @@
+#git clone https://github.com/pyenv/pyenv.git ~/.pyenv 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+# after then
+# pyenv install 2.7
+# cd /usr/local/bin && ln -s /home/cch/.pyenv/versions/2.7.18/bin/python2.7 ./python2.7
