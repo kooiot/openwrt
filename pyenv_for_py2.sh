919dd6bdc0fae9e04111a984b292ff28a3d6e84d
Dirk Chang <dirk@kooiot.com>
2023-12-04 18:40:48 +0800

    py2 script update

--- pyenv_for_py2.sh
+++ pyenv_for_py2.sh
@@ -3,5 +3,6 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 # after then
-# pyenv install 2.7
+# pyenv install 2.7.18
 # cd /usr/local/bin && ln -s /home/cch/.pyenv/versions/2.7.18/bin/python2.7 ./python2.7
+# pyenv global 2.7.18
