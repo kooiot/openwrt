7003101302cedcda31c03c73cca24db07fa755da
Dirk Chang <dirk@kooiot.com>
2026-09-11 20:32:40 +0800

    uboot-rockchip: support xtx spinand and sfc spi dll delay tuning

---
+++ target/linux/rockchip/armv7/base-files/lib/functions/tlink_csq_leds.sh
@@ -0,0 +1,164 @@
+#!/bin/sh
+
+. /lib/functions.sh
+
+SHOW=$1
+CSQ=$2
+SIGNAL=$3
+
+LED_PATH="/sys/class/leds/"
+LED_ORG_PATH="/sys/class/leds/kooiot:"
+
+tlink_led_echo()
+{
+	local name=$1
+	local value=$2
+
+	if [ -e ${LED_ORG_PATH}${name}/brightness ]
+	then
+		echo ${value} > ${LED_ORG_PATH}${name}/brightness
+	fi
+
+	if [ -e ${LED_PATH}${name}/brightness ]
+	then
+		echo ${value} > ${LED_PATH}${name}/brightness
+	fi
+}
+
+tlink_csq_leds_default()
+{
+	if [ ${SHOW} -le 0 ]; then
+		tlink_led_echo "green:bbs" 0
+		tlink_led_echo "green:bs" 0
+		tlink_led_echo "green:gs" 0
+		tlink_led_echo "green:ggs" 0
+		return 0
+	fi
+
+	if [ -n $SIGNAL ]; then
+		if [ $SIGNAL -ge 54 ]; then
+			tlink_led_echo "green:bbs" 255
+			tlink_led_echo "green:bs" 255
+			tlink_led_echo "green:gs" 255
+			if [ $SIGNAL -ge 83 ]; then
+				tlink_led_echo "green:ggs" 255
+			else
+				tlink_led_echo "green:ggs" 0
+			fi
+		else
+			tlink_led_echo "green:ggs" 0
+			tlink_led_echo "green:gs" 0
+			tlink_led_echo "green:bbs" 255
+			if [ $SIGNAL -ge 32 ]; then
+				tlink_led_echo "green:bs" 255
+			else
+				tlink_led_echo "green:bs" 0
+			fi
+		fi
+	else
+		if [ $CSQ -ge 17 ]; then
+			tlink_led_echo "green:bbs" 255
+			tlink_led_echo "green:bs" 255
+			tlink_led_echo "green:gs" 255
+			if [ $CSQ -ge 26 ]; then
+				tlink_led_echo "green:ggs" 255
+			else
+				tlink_led_echo "green:ggs" 0
+			fi
+		else
+			tlink_led_echo "green:ggs" 0
+			tlink_led_echo "green:gs" 0
+			tlink_led_echo "green:bbs" 255
+			if [ $CSQ -ge 10 ]; then
+				tlink_led_echo "green:bs" 255
+			else
+				tlink_led_echo "green:bs" 0
+			fi
+		fi
+	fi
+}
+
+tlink_csq_leds_mixed()
+{
+	if [ ${SHOW} -le 0 ]; then
+		tlink_led_echo "green:gs" 0
+		tlink_led_echo "yellow:bs" 0
+		return 0
+	fi
+	if [ -n $SIGNAL ]; then
+		if [ $SIGNAL -ge 54 ]; then
+			tlink_led_echo "green:gs" 255
+			tlink_led_echo "yellow:bs" 0
+		else
+			tlink_led_echo "yellow:bs" 255
+			if [ $SIGNAL -ge 32 ]; then
+				tlink_led_echo "green:gs" 255
+			else
+				tlink_led_echo "yellow:bs" 0
+			fi
+		fi
+	else
+		if [ $CSQ -ge 17 ]; then
+			tlink_led_echo "green:gs" 255
+			tlink_led_echo "yellow:bs" 0
+		else
+			tlink_led_echo "yellow:bs" 1
+			if [ $CSQ -ge 10 ]; then
+				tlink_led_echo "green:gs" 255
+			else
+				tlink_led_echo "yellow:bs" 0
+			fi
+		fi
+	fi
+}
+
+
+tlink_csq_leds_single()
+{
+	if [ ${SHOW} -le 0 ]; then
+		echo "none" > /sys/class/leds/kooiot:green:csq/trigger
+		echo 0 > /sys/class/leds/kooiot:green:csq/brightness
+		return 0
+	fi
+	if [ -n $SIGNAL ]; then
+		if [ $SIGNAL -ge 54 ]; then
+			echo "none" > /sys/class/leds/kooiot:green:csq/trigger
+			echo 255 > /sys/class/leds/kooiot:green:csq/brightness
+		else
+			echo "timer" > /sys/class/leds/kooiot:green:csq/trigger
+			echo 500 > /sys/class/leds/kooiot:green:csq/delay_on
+			if [ $SIGNAL -ge 32 ]; then
+				echo 500 > /sys/class/leds/kooiot:green:csq/delay_off
+			else
+				echo 2000 > /sys/class/leds/kooiot:green:csq/delay_off
+			fi
+		fi
+	else
+		if [ $CSQ -ge 17 ]; then
+			echo "none" > /sys/class/leds/kooiot:green:csq/trigger
+			echo 255 > /sys/class/leds/kooiot:green:csq/brightness
+		else
+			echo "timer" > /sys/class/leds/kooiot:green:csq/trigger
+			echo 500 > /sys/class/leds/kooiot:green:csq/delay_on
+			if [ $CSQ -ge 10 ]; then
+				echo 500 > /sys/class/leds/kooiot:green:csq/delay_off
+			else
+				echo 2000 > /sys/class/leds/kooiot:green:csq/delay_off
+			fi
+		fi
+	fi
+}
+
+case $(board_name) in
+	kooiot,tlink-r4x)
+		tlink_csq_leds_mixed
+		;;
+	kooiot,tlink-r7)
+		tlink_csq_leds_single
+		;;
+	*)
+		tlink_csq_leds_default
+		;;
+esac
+
+exit 0
