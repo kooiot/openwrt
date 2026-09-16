0da74857f54ca65004de465483acf20a04e84b34
Dirk Chang <dirk@kooiot.com>
2026-09-16 09:49:30 +0800

    uboot-rockchip: disable auto resize ubi volume

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -517,7 +517,7 @@ define Device/kooiot_tlink-k1
     kmod-usb-serial kmod-usb-serial-option \
     kmod-usb-serial-qualcomm kmod-usb-net-qmi-wwan \
     kmod-serial-wk2xxx-spi kmod-eeprom-at24 \
-    kmod-phy-motorcomm kmod-motorcomm-yt8512c \
+    kmod-phy-motorcomm \
     uqmi fdisk usbutils freeioe wwan-watch \
     luci-app-freeioe \
 	luci-proto-qmi luci-proto-3g luci-proto-ncm luci-proto-wwan \
@@ -540,7 +540,7 @@ define Device/kooiot_tlink-k2
     kmod-usb-serial kmod-usb-serial-option \
     kmod-usb-serial-qualcomm kmod-usb-net-qmi-wwan \
     kmod-usb-net-asix kmod-serial-wk2xxx-spi \
-    kmod-phy-motorcomm kmod-motorcomm-yt8512c \
+    kmod-phy-motorcomm \
     uqmi fdisk usbutils freeioe wwan-watch \
     luci-app-freeioe \
 	luci-proto-qmi luci-proto-3g luci-proto-ncm luci-proto-wwan \
@@ -566,7 +566,7 @@ define Device/kooiot_tlink-k2x
     kmod-usb-net-asix kmod-serial-wk2xxx-spi \
 	kmod-can kmod-can-mcp251x \
 	kmod-can-bcm kmod-can-raw ip-full\
-    kmod-phy-motorcomm kmod-motorcomm-yt8512c \
+    kmod-phy-motorcomm \
     uqmi fdisk usbutils freeioe wwan-watch \
 	luci-proto-qmi luci-proto-3g luci-proto-ncm luci-proto-wwan \
 	luci-app-freeioe tinc-freeioe-tunnel \
@@ -593,7 +593,7 @@ define Device/kooiot_tlink-k4a
     kmod-usb-serial-qualcomm kmod-usb-net-qmi-wwan \
     kmod-serial-ch9434 kmod-can kmod-can-mcp251x \
     kmod-can-bcm kmod-can-raw ip-full\
-    kmod-phy-motorcomm kmod-motorcomm-yt8512c \
+    kmod-phy-motorcomm \
     uqmi fdisk usbutils freeioe wwan-watch \
 	luci-proto-qmi luci-proto-3g luci-proto-ncm luci-proto-wwan \
     luci-app-freeioe tinc-freeioe-tunnel\
@@ -620,7 +620,7 @@ define Device/kooiot_tlink-k4g
     kmod-usb-serial-qualcomm kmod-usb-net-qmi-wwan \
     kmod-serial-ch9434 kmod-can kmod-can-mcp251x \
     kmod-can-bcm kmod-can-raw ip-full\
-    kmod-phy-motorcomm kmod-motorcomm-yt8512c \
+    kmod-phy-motorcomm \
     uqmi fdisk usbutils freeioe wwan-watch \
 	luci-proto-qmi luci-proto-3g luci-proto-ncm luci-proto-wwan \
     luci-app-freeioe tinc-freeioe-tunnel\
